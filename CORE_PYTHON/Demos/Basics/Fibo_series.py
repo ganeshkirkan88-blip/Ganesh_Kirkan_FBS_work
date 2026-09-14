92e636e85941a1ab0d13b59110d547133552d52c
ganeshkirkan88-blip <ganeshkirkan88@gmail.com>
2026-09-14 12:03:23 +0530

    Add files via upload

---
+++ CORE_PYTHON/Demos/Basics/Fibo_series.py
@@ -0,0 +1,16 @@
+num =int(input('How many  fibonacci number you want :'))
+
+a=-1
+b= 1
+
+for i in range(num):
+    c = a+b
+    print(c, end = '_')
+    a=b
+    b=c
+
+
+
+
+
+   
---
+++ CORE_PYTHON/Demos/Basics/Range_demo.py
@@ -0,0 +1,18 @@
+#for i in range (2, 20 , 2):
+#for i in range (5 , 51 ,5):
+
+'''n = int(input('Enter number :'))
+for i in range(n,n*10+1,-n):
+    print(i)
+'''
+
+'''for i in range (n, n *10+1 ,n):
+  for i in range (20 , 1 ,-2):
+    for i in range(n*10,n-1,-n):
+     print(i)
+'''
+
+'''for i in range(1,11):
+  for i in range(5):
+    print(i)
+'''
