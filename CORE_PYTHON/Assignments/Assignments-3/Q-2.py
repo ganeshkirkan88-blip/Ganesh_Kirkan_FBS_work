18b538a07f90c61e9580f3d3b2e4c57f9650cb49
ganeshkirkan88-blip <ganeshkirkan88@gmail.com>
2026-09-15 16:15:47 +0530

    Delete CORE_PYTHON/Assignments/Assignments-3 directory

--- CORE_PYTHON/Assignments/Assignments-3/Q-2.py
+++
@@ -1,8 +0,0 @@
-#2. Write a program to input any alphabet and check whether it is vowel or consonant.
-
-char = input('Enter the charecter:')
-
-if(char in 'aeiouAEIOU'):
-   print(f'{char} is vowel.')
-else:
-   print(f'{char} is constant.')
--- CORE_PYTHON/Assignments/Assignments-3/Q-7.py
+++
@@ -1,9 +0,0 @@
-#7. Write a program to check if user has entered correct userid and password.
-
-user_id = input('Enter user id :' )
-password = input('Enter user password :')
-
-if(user_id == 'ganesh'and password == '1234'):
-    print('Login successfully')
-else:
-    print('wrong user_id and password')
